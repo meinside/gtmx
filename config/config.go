ce9a643662c7904bb2e542067f020a94c5197f08
Sungjin Han <meinside@st-company.net>
2015-07-27 12:30:29 +0900

    refactor config functionality

---
+++ config/config.go
@@ -0,0 +1,158 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	CONFIG_FILENAME = ".gtmx.json" // config file's name
+)
+
+type SessionConfig struct {
+	SessionName string         `json:"session_name"`
+	Windows     []WindowConfig `json:"windows"`
+	Focus       FocusConfig    `json:"focus"`
+}
+type WindowConfig struct {
+	Name    string      `json:"name"`
+	Command string      `json:"cmd"`
+	Split   SplitConfig `json:"split"`
+}
+type SplitConfig struct {
+	Vertical   bool         `json:"vertical"`
+	Percentage int          `json:"percentage"`
+	Panes      []PaneConfig `json:"panes"`
+}
+type PaneConfig struct {
+	Pane    string `json:"pane"`
+	Command string `json:"cmd"`
+}
+type FocusConfig struct {
+	Name string `json:"name"`
+	Pane string `json:"pane"`
+}
+
+/*
+	Read all predefined session configs from file
+*/
+func ReadAll() map[string]SessionConfig {
+	all := make(map[string]SessionConfig)
+
+	if user, err := user.Current(); err != nil {
+		fmt.Printf("* Failed to get current user (%s)\n", err)
+
+		os.Exit(1)
+	} else {
+		configFilepath := fmt.Sprintf("%s/%s", user.HomeDir, CONFIG_FILENAME)
+
+		if file, err := ioutil.ReadFile(configFilepath); err != nil {
+			fmt.Printf("* Failed to read config file (%s)\n", err)
+		} else {
+			if err := json.Unmarshal(file, &all); err != nil {
+				fmt.Printf("* Failed to parse config file (%s)\n", err)
+			}
+		}
+	}
+
+	return all
+}
+
+/*
+	Get sample config (for generating sample config file)
+*/
+func GetSampleConfig() map[string]SessionConfig {
+	sample := make(map[string]SessionConfig)
+
+	sample["rails"] = SessionConfig{
+		SessionName: "%d",
+		Windows: []WindowConfig{
+			{
+				Name: "console",
+			},
+			{
+				Name:    "models",
+				Command: "cd ./app/models; clear",
+			},
+			{
+				Name:    "views",
+				Command: "cd ./app/views; clear",
+			},
+			{
+				Name:    "controllers",
+				Command: "cd ./app/controllers; clear",
+			},
+			{
+				Name:    "configs",
+				Command: "cd ./config; clear",
+			},
+			{
+				Name:    "server",
+				Command: "",
+				Split: SplitConfig{
+					Vertical:   true,
+					Percentage: 50,
+					Panes: []PaneConfig{
+						{
+							Pane:    "1",
+							Command: "rails server",
+						},
+						{
+							Pane:    "2",
+							Command: "rails console",
+						},
+					},
+				},
+			},
+		},
+		Focus: FocusConfig{
+			Name: "server",
+			Pane: "2",
+		},
+	}
+
+	return sample
+}
+
+/*
+	Get sample config as JSON
+*/
+func GetSampleConfigAsJSON() string {
+	sample := GetSampleConfig()
+	if b, err := json.MarshalIndent(sample, "", "    "); err == nil {
+		return string(b)
+	}
+	return "{}"
+}
+
+/*
+	Replace string with place holders
+
+	'%d' => current directory's name
+	'%h' => hostname of this machine
+*/
+func ReplaceString(str string) string {
+	replaced := str
+
+	// '%d' => current directory name
+	if strings.Contains(replaced, "%d") {
+		if dir, err := os.Getwd(); err == nil {
+			replaced = strings.Replace(replaced, "%d", filepath.Base(dir), -1)
+		}
+	}
+
+	// '%h' => host name
+	if strings.Contains(replaced, "%h") {
+		if output, err := exec.Command("hostname", "-s").CombinedOutput(); err == nil {
+			replaced = strings.Replace(replaced, "%h", strings.TrimSpace(string(output)), 0)
+		}
+	}
+
+	return replaced
+}
--- gtmx.go
+++ gtmx.go
@@ -1,158 +1,134 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	config "github.com/meinside/gtmx/config"
 	tmux "github.com/meinside/gtmx/helper"
-	"io/ioutil"
 	"os"
-	"os/exec"
-	"os/user"
-	"path/filepath"
 	"strconv"
-	"strings"
 )
 
-const (
-	CONFIG_FILENAME = ".gtmx.json"
-)
-
-type SessionConfig struct {
-	SessionName string         `json:"session_name"`
-	Windows     []WindowConfig `json:"windows"`
-	Focus       FocusConfig    `json:"focus"`
-}
-type WindowConfig struct {
-	Name    string      `json:"name"`
-	Command string      `json:"cmd"`
-	Split   SplitConfig `json:"split"`
-}
-type SplitConfig struct {
-	Vertical   bool         `json:"vertical"`
-	Percentage int          `json:"percentage"`
-	Panes      []PaneConfig `json:"panes"`
-}
-type PaneConfig struct {
-	Pane    string `json:"pane"`
-	Command string `json:"cmd"`
-}
-type FocusConfig struct {
-	Name string `json:"name"`
-	Pane string `json:"pane"`
-}
-
-func replaceString(sessionName string) string {
-	replaced := sessionName
-
-	// '%d' => current directory name
-	if strings.Contains(replaced, "%d") {
-		if dir, err := os.Getwd(); err == nil {
-			replaced = strings.Replace(replaced, "%d", filepath.Base(dir), -1)
+func paramExists(params []string, shortParam string, longParam string) bool {
+	for _, param := range params {
+		if param == shortParam || param == longParam {
+			return true
 		}
 	}
+	return false
+}
 
-	// '%h' => host name
-	if strings.Contains(replaced, "%h") {
-		if output, err := exec.Command("hostname", "-s").CombinedOutput(); err == nil {
-			replaced = strings.Replace(replaced, "%h", strings.TrimSpace(string(output)), 0)
-		}
-	}
+func printUsage() {
+	fmt.Printf(`> Usage
 
-	return replaced
+# show this help message
+
+$ gtmx -h
+$ gtmx --help
+
+
+# show sample config
+
+$ gtmx -g
+$ gtmx --gen-config
+
+
+# start or resume (predefined) session
+
+$ gtmx [SESSION_NAME]
+`)
+
+	os.Exit(0)
+}
+
+func generateConfig() {
+	sample := config.GetSampleConfigAsJSON()
+
+	fmt.Printf("> Sample config (save it as ~/%s)\n\n", config.CONFIG_FILENAME)
+
+	fmt.Println(sample)
+
+	os.Exit(0)
 }
 
 func main() {
-	if user, err := user.Current(); err != nil {
-		fmt.Printf("* Failed to get current user (%s)\n", err)
-	} else {
-		helper := tmux.NewHelper()
+	params := os.Args[1:]
 
-		var sessionName string
-		params := os.Args[1:]
-		if len(params) > 0 {
-			sessionName = params[0]
-		} else {
-			sessionName = ""
-		}
-
-		configFilepath := fmt.Sprintf("%s/%s", user.HomeDir, CONFIG_FILENAME)
-		if file, err := ioutil.ReadFile(configFilepath); err == nil {
-			var config map[string]SessionConfig
-			if err := json.Unmarshal(file, &config); err != nil {
-				fmt.Printf("* Failed to read config file (%s)\n", err)
-			}
-
-			if selectedConf, ok := config[sessionName]; ok {
-				fmt.Printf("> Using predefined session: %s\n", sessionName)
-
-				selectedConf.SessionName = replaceString(selectedConf.SessionName)
-
-				if !tmux.IsSessionCreated(selectedConf.SessionName) {
-					helper.StartSession(selectedConf.SessionName)
-
-					for _, window := range selectedConf.Windows {
-						// window name
-						windowName := replaceString(window.Name)
-
-						// window command
-						windowCommand := replaceString(window.Command)
-
-						// create window with given name and command
-						helper.CreateWindow(windowName, windowCommand)
-
-						// split panes
-						if window.Split.Percentage > 0 {
-							helper.SplitWindow(windowName, map[string]string{
-								"vertical":   strconv.FormatBool(window.Split.Vertical),
-								"percentage": strconv.Itoa(window.Split.Percentage),
-							})
-							for _, pane := range window.Split.Panes {
-								helper.Command(windowName, pane.Pane, replaceString(pane.Command))
-							}
-						}
-					}
-
-					// focus window/pane
-					if selectedConf.Focus.Name != "" {
-						focusedWindow := selectedConf.Focus.Name
-						focusedPane := selectedConf.Focus.Pane
-
-						if focusedWindow != "" {
-							helper.FocusWindow(focusedWindow)
-							if focusedPane != "" {
-								helper.FocusPane(focusedPane)
-							}
-						}
-					}
-				} else {
-					fmt.Printf("> Resuming session: %s\n", sessionName)
-
-					helper.StartSession(sessionName)
-				}
-			} else {
-				helper.StartSession(sessionName)
-
-				if !tmux.IsSessionCreated(sessionName) {
-					fmt.Printf("> No matching predefined session, creating a new session: %s\n", sessionName)
-
-					helper.CreateWindow(tmux.DEFAULT_WINDOW_NAME, "")
-				} else {
-					fmt.Printf("> No matching predefined session, resuming session: %s\n", sessionName)
-				}
-			}
-		} else {
-			helper.StartSession(sessionName)
-
-			if !tmux.IsSessionCreated(sessionName) {
-				fmt.Printf("> Not using predefined session, creating a new session: %s\n", sessionName)
-
-				helper.CreateWindow(tmux.DEFAULT_WINDOW_NAME, "")
-			} else {
-				fmt.Printf("> Not using predefined session, resuming session: %s\n", sessionName)
-			}
-		}
-
-		//attach
-		helper.Attach()
+	// check params
+	if paramExists(params, "-h", "--help") {
+		printUsage()
+	} else if paramExists(params, "-g", "--gen-config") {
+		generateConfig()
 	}
+
+	var sessionName string
+	if len(params) > 0 {
+		sessionName = params[0]
+	} else {
+		sessionName = ""
+	}
+
+	helper := tmux.NewHelper()
+
+	configs := config.ReadAll()
+	if selected, ok := configs[sessionName]; ok {
+		fmt.Printf("> Using predefined session: %s\n", sessionName)
+
+		selected.SessionName = config.ReplaceString(selected.SessionName)
+
+		if !tmux.IsSessionCreated(selected.SessionName) {
+			helper.StartSession(selected.SessionName)
+
+			for _, window := range selected.Windows {
+				// window name
+				windowName := config.ReplaceString(window.Name)
+
+				// window command
+				windowCommand := config.ReplaceString(window.Command)
+
+				// create window with given name and command
+				helper.CreateWindow(windowName, windowCommand)
+
+				// split panes
+				if window.Split.Percentage > 0 {
+					helper.SplitWindow(windowName, map[string]string{
+						"vertical":   strconv.FormatBool(window.Split.Vertical),
+						"percentage": strconv.Itoa(window.Split.Percentage),
+					})
+					for _, pane := range window.Split.Panes {
+						helper.Command(windowName, pane.Pane, config.ReplaceString(pane.Command))
+					}
+				}
+			}
+
+			// focus window/pane
+			if selected.Focus.Name != "" {
+				focusedWindow := selected.Focus.Name
+				focusedPane := selected.Focus.Pane
+
+				if focusedWindow != "" {
+					helper.FocusWindow(focusedWindow)
+					if focusedPane != "" {
+						helper.FocusPane(focusedPane)
+					}
+				}
+			}
+		} else {
+			fmt.Printf("> Resuming session: %s\n", sessionName)
+
+			helper.StartSession(sessionName)
+		}
+	} else {
+		helper.StartSession(sessionName)
+
+		if !tmux.IsSessionCreated(sessionName) {
+			fmt.Printf("> No matching predefined session, creating a new session: %s\n", sessionName)
+
+			helper.CreateWindow(tmux.DEFAULT_WINDOW_NAME, "")
+		} else {
+			fmt.Printf("> No matching predefined session, resuming session: %s\n", sessionName)
+		}
+	}
+
+	//attach
+	helper.Attach()
 }
